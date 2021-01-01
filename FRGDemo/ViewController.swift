686d42a6e8a5161eafd4f367a96cff512df18d0e
Christian Hurtado <christian.hurtado.catacoli@gmail.com>
2021-01-01 04:01:50 -0500

    Initial Commit

---
+++ FRGDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FRGDemo
+//
+//  Created by Christian Hurtado on 1/1/21.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
