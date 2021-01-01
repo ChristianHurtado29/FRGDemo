49a082c267304e25fb18d4f8d9c3aa72f3e555d1
Christian Hurtado <christian.hurtado.catacoli@gmail.com>
2021-01-01 04:06:32 -0500

    added calendar via pods and learning delegate

--- FRGDemo/ViewController.swift
+++ FRGDemo/ViewController.swift
@@ -6,15 +6,23 @@
 //
 
 import UIKit
+import FSCalendar
 
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var calendar: FSCalendar!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        calendar.delegate = self
     }
 
 
 }
 
+extension ViewController: FSCalendarDelegate {
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        print(date)
+    }
+}
